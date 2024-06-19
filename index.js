baa1661940eb0b6084dd791b75a7bc7acb3bfd39
Winicius Pichirilo <winy162010@hotmail.com>
2024-06-19 20:33:26 -0300

    projeto proposto Felipão da dio3

--- index.js
+++ index.js
@@ -1,41 +1,38 @@
-const heroes = [
-    { name: 'Kratos', xp: 200 },
-    { name: 'Zeus', xp: 300 },
-    { name: 'Hades', xp: 250 },
-    { name: 'Athena', xp: 1500 },
-    { name: 'Ares', xp: 2200 },
-    { name: 'Gaia', xp: 3500 }
+const herois = [
+    { nome: 'Kratos', xp: 200 },
+    { nome: 'Zeus', xp: 300 },
+    { nome: 'Hades', xp: 250 },
+    { nome: 'Athena', xp: 1500 },
+    { nome: 'Ares', xp: 2200 },
+    { nome: 'Gaia', xp: 3500 }
   ];
   
-  function getTotalPoints(hero) {
-    return hero.xp;
-  }
-  
-  function getLevel(hero) {
-    const xp = getTotalPoints(hero);
-  
-    let level;
+  function calcularNivel(xp) {
+    let nivel;
   
     if (xp < 1000) {
-      level = 'Ferro';
+      nivel = 'Ferro';
     } else if (xp >= 1001 && xp <= 2000) {
-      level = 'Bronze';
-    } else if (xp >= 2001 && xp <= 3000) {
-      level = 'Prata';
-    } else if (xp >= 3001 && xp <= 4000) {
-      level = 'Ouro';
-    } else if (xp >= 4001 && xp <= 5000) {
-      level = 'Platina Diamante';
-    } else if (xp >= 5001 && xp <= 6000) {
-      level = 'Ascendente';
-    } else if (xp >= 6001 && xp <= 7000) {
-      level = 'Imortal';
+      nivel = 'Bronze';
+    } else if (xp >= 2001 && xp <= 5000) {
+      nivel = 'Prata';
+    } else if (xp >= 5001 && xp <= 7000) {
+      nivel = 'Ouro';
+    } else if (xp >= 7001 && xp <= 8000) {
+      nivel = 'Platina';
+    } else if (xp >= 8001 && xp <= 9000) {
+      nivel = 'Ascendente';
+    } else if (xp >= 9001 && xp <= 10000) {
+      nivel = 'Imortal';
+    } else {
+      nivel = 'Radiante';
     }
   
-    return level;
+    return nivel;
   }
   
-  heroes.forEach(hero => {
-    console.log(`${hero.name} é ${getLevel(hero)}`);
+  herois.forEach(heroi => {
+    const nivel = calcularNivel(heroi.xp);
+    console.log(`O Herói de nome ${heroi.nome} está no nível de ${nivel}`);
   });
   
