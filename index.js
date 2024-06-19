f98d719ed9919258ef95317905627c81f07c36da
Winicius Pichirilo <winy162010@hotmail.com>
2024-06-19 20:27:07 -0300

    projeto proposto Felipão da dio2

--- index.js
+++ index.js
@@ -1,27 +1,41 @@
-const names = ['Kratos', 'Zeus', 'Hades'];
-const powerPoints = [70, 100, 80];
-const defensePoints = [75, 90, 70];
-const speedPoints = [65, 85, 60];
-const experiencePoints = [80, 95, 90];
-
-function getTotalPoints(index) {
-  return powerPoints[index] + defensePoints[index] + speedPoints[index] + experiencePoints[index];
-}
-
-function getLevel(index) {
-  const totalPoints = getTotalPoints(index);
-
-  if (totalPoints <= 100) {
-    return 'Nível 1: Herói Iniciante';
-  } else if (totalPoints <= 200) {
-    return 'Nível 2: Herói Intermediário';
-  } else if (totalPoints <= 300) {
-    return 'Nível 3: Herói Avançado';
-  } else {
-    return 'Nível 4: Herói Mestre';
+const heroes = [
+    { name: 'Kratos', xp: 200 },
+    { name: 'Zeus', xp: 300 },
+    { name: 'Hades', xp: 250 },
+    { name: 'Athena', xp: 1500 },
+    { name: 'Ares', xp: 2200 },
+    { name: 'Gaia', xp: 3500 }
+  ];
+  
+  function getTotalPoints(hero) {
+    return hero.xp;
   }
-}
-
-for (let i = 0; i < names.length; i++) {
-  console.log(`${names[i]} é ${getLevel(i)}`);
-}
+  
+  function getLevel(hero) {
+    const xp = getTotalPoints(hero);
+  
+    let level;
+  
+    if (xp < 1000) {
+      level = 'Ferro';
+    } else if (xp >= 1001 && xp <= 2000) {
+      level = 'Bronze';
+    } else if (xp >= 2001 && xp <= 3000) {
+      level = 'Prata';
+    } else if (xp >= 3001 && xp <= 4000) {
+      level = 'Ouro';
+    } else if (xp >= 4001 && xp <= 5000) {
+      level = 'Platina Diamante';
+    } else if (xp >= 5001 && xp <= 6000) {
+      level = 'Ascendente';
+    } else if (xp >= 6001 && xp <= 7000) {
+      level = 'Imortal';
+    }
+  
+    return level;
+  }
+  
+  heroes.forEach(hero => {
+    console.log(`${hero.name} é ${getLevel(hero)}`);
+  });
+  
